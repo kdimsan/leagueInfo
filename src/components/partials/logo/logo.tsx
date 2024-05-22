8b0a8c7e513d043b4cc8b45e8aaf8280ca5d345e
kdimsan <kdimsantana@gmail.com>
2024-05-22 13:05:09 +0100

    feat: add logo section

---
+++ src/components/partials/logo/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="flex justify-center items-center w-fit gap-1">
+      <Image width={50} height={50} src="/logo.png" alt="Logo" />
+      <h1 className="font-bold font-title text-2xl tracking-widest whitespace-nowrap lg:text-3xl">
+        League Finder
+      </h1>
+    </div>
+  );
+}

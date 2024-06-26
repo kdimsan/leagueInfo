c47299f7bde79ce0b96b9efde58c555528e3a99d
kdimsan <kdimsantana@gmail.com>
2024-06-26 16:51:34 +0100

    ref:  improve jsx elements

--- src/components/championsSection/allChampionsSection.tsx
+++ src/components/championsSection/allChampionsSection.tsx
@@ -25,7 +25,7 @@ export default function AllChampionsSection({
         })
       : data;
   return (
-    <div className="flex flex-col justify-center items-center default-bg py-5">
+    <section className="flex flex-col justify-center items-center default-bg py-5">
       <TitleConfig
         className={"text-2xl tracking-wider md:text-3xl"}
         title={"League of Legends Champions"}
@@ -38,6 +38,6 @@ export default function AllChampionsSection({
           return <ChampionSquareCard key={index} data={champion} />;
         })}
       </div>
-    </div>
+    </section>
   );
 }
